302b9ea44aade79da570800bbb38b10fc62b6f45
ruthvikjmdraup <122518681+ruthvikjmdraup@users.noreply.github.com>
2023-02-01 17:26:21 +0530

    added exception handling

--- resources/routes.py
+++ resources/routes.py
@@ -1,10 +1,10 @@
-from resources.user import UsersApi, UserApi, OneUserApi
+from resources.user import UsersApi, UpdateUserApi, OneUserApi
 from resources.auth import SignupApi, LoginApi
 
 
 def initialize_routes(api):
     api.add_resource(UsersApi, '/api/users')
-    api.add_resource(UserApi, '/api/user/<_id>')
+    api.add_resource(UpdateUserApi, '/api/user/<_id>')
     api.add_resource(OneUserApi, '/api/oneUser/<_id>')
     api.add_resource(SignupApi, '/api/auth/signup')
     api.add_resource(LoginApi, '/api/auth/login')
--- resources/user.py
+++ resources/user.py
@@ -2,6 +2,9 @@ from flask import Response, request
 from database.models import Details, User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
+from resources.errors import SchemaValidationError, DetailsAlreadyExistsError, InternalServerError, \
+     UpdatingDetailsError, DeletingDetailsError, DetailsNotExistsError
 
 
 class UsersApi(Resource):
@@ -17,42 +20,62 @@ class UsersApi(Resource):
 
     @jwt_required()
     def post(self):
-        identity = get_jwt_identity()
-        admin = User.objects.get(id=identity, role='admin')
-        body = request.get_json()
-        get_details = Details(**body, added_by=admin)
-        get_details.save()
-        admin.save()
-        _id = get_details.id
-        return {'id': str(_id)}, 200
+        try:
+            identity = get_jwt_identity()
+            admin = User.objects.get(id=identity, role='admin')
+            body = request.get_json()
+            get_details = Details(**body, added_by=admin)
+            get_details.save()
+            admin.save()
+            _id = get_details.id
+            return {'id': str(_id)}, 200
+        except (FieldDoesNotExist, ValidationError):
+            raise SchemaValidationError
+        except NotUniqueError:
+            raise DetailsAlreadyExistsError
+        except Exception:
+            raise InternalServerError
 
 
-class UserApi(Resource):
+class UpdateUserApi(Resource):
     @jwt_required()
     def put(self, _id):
-        identity = get_jwt_identity()
-        admin = User.objects.get(role="admin")
-        if admin["role"] == "admin":
-            body = request.get_json()
-            Details.objects.get(id=_id, added_by=identity).update(**body)
-            return 'Updated', 200
-        else:
-            return 'Not Updated', 500
+        try:
+            identity = get_jwt_identity()
+            admin = User.objects.get(role="admin")
+            if admin["role"] == "admin":
+                body = request.get_json()
+                Details.objects.get(id=_id, added_by=identity).update(**body)
+                return 'Updated', 200
+            else:
+                return 'Not Updated', 500
+        except InvalidQueryError:
+            raise SchemaValidationError
+        except DoesNotExist:
+            raise UpdatingDetailsError
 
     @jwt_required()
     def delete(self, _id):
-        user_id = get_jwt_identity()
-        admin = User.objects.get(role="admin")
-        if admin["role"] == "admin":
-            user = Details.objects.get(id=_id, added_by=user_id)
-            user.delete()
-            return 'deleted', 200
-        else:
-            return 'Only admin can delete', 500
+        try:
+            user_id = get_jwt_identity()
+            admin = User.objects.get(role="admin")
+            if admin["role"] == "admin":
+                user = Details.objects.get(id=_id, added_by=user_id)
+                user.delete()
+                return 'deleted', 200
+            else:
+                return 'Only admin can delete', 500
+        except DoesNotExist:
+            raise DeletingDetailsError
 
 
 class OneUserApi(Resource):
     @jwt_required()
     def get(self, _id):
-        users = Details.objects.get(id=_id).to_json()
-        return Response(users, mimetype="application/json", status=200)
+        try:
+            users = Details.objects.get(id=_id).to_json()
+            return Response(users, mimetype="application/json", status=200)
+        except DoesNotExist:
+            raise DetailsNotExistsError
+        except Exception:
+            raise InternalServerError
