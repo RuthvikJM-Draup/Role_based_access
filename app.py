f39474c2a81b67c671cec60feb6f2cfe7bbcfba3
ruthvikjmdraup <122518681+ruthvikjmdraup@users.noreply.github.com>
2023-02-01 18:06:51 +0530

    added error handling

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from flask_jwt_extended import JWTManager
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from resources.errors import errors
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 't1NP63m4wnBg6nyHYKmc2TpCORGI4nss'
 
-api = Api(app)
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
